f9c82c31d0433b7dd48d603781147c7958fc66d0
qddnovo <quxingyi@outlook.com>
2019-03-19 15:24:50 +0800

    Initial Commit

---
+++ AutoPropertyCocoaDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoPropertyCocoaDemo
+//
+//  Created by MDLK on 2019/3/19.
+//  Copyright © 2019 Novo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoPropertyCocoaDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoPropertyCocoaDemo
+//
+//  Created by MDLK on 2019/3/19.
+//  Copyright © 2019 Novo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
